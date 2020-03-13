958a95156c79a614698f3df4f21edf94a7adb604
PHILMIZ <mizeroa17@mytru.ca>
2020-03-12 17:58:56 -0700

    Is it working??

---
+++ app/src/main/java/com/example/memento/Create_Daily.java
@@ -0,0 +1,14 @@
+package com.example.memento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Create_Daily extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create__daily);
+    }
+}
