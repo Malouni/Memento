7b38cf5b8493090a056230917f731e43e7332b8d
PHILMIZ <mizeroa17@mytru.ca>
2020-03-12 19:55:09 -0700

    Is it working??

---
+++ app/src/main/java/com/example/memento/Daily_Reminder.java
@@ -0,0 +1,14 @@
+package com.example.memento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Daily_Reminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily__reminder);
+    }
+}
---
+++ app/src/main/java/com/example/memento/Study_Work_Reminders.java
@@ -0,0 +1,14 @@
+package com.example.memento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Study_Work_Reminders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study__work__reminders);
+    }
+}
