ce228a784193053618665ddd6a4d25fb2c74d227
malouni <swarealy@gmail.com>
2020-03-12 18:18:42 -0700

    sd

--- app/src/main/java/com/example/memento/MainActivity.java
+++ app/src/main/java/com/example/memento/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.memento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,7 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
 
             case R.id.create_reminder:
-
+                Intent intent = new Intent(MainActivity.this,Create_reminder.class);
+                startActivity(intent);
                 break;
 
             case R.id.study_work:
